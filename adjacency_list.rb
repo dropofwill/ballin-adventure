484699960afb7582366149d2ebabff2b03180f93
Will <whp3652@rit.edu>
2014-06-15 16:19:58 -0400

    progress on prune mode

--- adjacency_list.rb
+++ adjacency_list.rb
@@ -1,6 +1,6 @@
 class AdjList
-	def initialize array_of_vertices, directed: false, broken_links: :ignore, weighted: false
-		@adj_list = array_of_vertices
+	def initialize vertex_hash, directed: false, broken_links: :ignore, weighted: false
+		@adj_list = vertex_hash 
 		@directed = directed
 		@weighted = weighted
 		@handle_broken_links = broken_links
@@ -10,15 +10,15 @@ class AdjList
 	end
 
 	def valid_edges?
-		@adj_list.each do |vertex|
-			current_vertex = vertex[0]
-			vertex.each do |adj|
-				# p "Vertex #{current_vertex}, Edge to Vertex #{adj}"
+		@adj_list.each do |vertex, vertex_list|
+			vertex_list.each do |adj|
+				 #p "Vertex #{vertex}, Edge to Vertex #{adj}"
 				
-				if @adj_list[adj].nil?
-					handle_broken_links current_vertex, adj, :missing_vertex
-				elsif !@directed && !@adj_list[adj].include?(current_vertex)
-					handle_broken_links current_vertex, adj, :missing_ref
+				if !@adj_list.has_key? adj
+					p "Missing: #{vertex} => #{adj}"
+					handle_broken_links vertex, adj, :missing_vertex
+				elsif !@directed && !@adj_list[adj].include?(vertex)
+					handle_broken_links vertex, adj, :missing_ref
 				end
 			end
 		end
@@ -37,18 +37,30 @@ class AdjList
 			end
 		when :fix
 		when :prune
+			if type == :missing_vertex
+				p "Missing: Deleted #{vertex} => #{adj_vertex}"
+				@adj_list[vertex].delete(adj_vertex)
+				@broken_links[vertex] = adj_vertex
+			elsif type == :missing_ref
+				p "Cross Ref: Deleted #{vertex} => #{adj_vertex}"
+				#@adj_list[vertex].delete(adj_vertex)
+				#@broken_links[vertex] = adj_vertex
+			end
 		end
 	end
 end
 
-# p AdjList.new [ [0,1], [1,0,2,3], [2,1,4], [3,0,1] ]
+#p AdjList.new({ 0 => [1], 1 => [0,2,3], 2 => [1], 3 => [0,1] })
 
-data = []
-File.open("kargerMinCut_data.txt").each_line do |line|
-	vertex = []
+vertices = {}
+File.open("kargerMinCut_data_simple.txt").each_line do |line|
 	vertex = line.gsub(/\s+/, ' ').strip.split(" ")
 	vertex.map! { |i| i.to_i }
-	data << vertex
+	c_v = vertex.shift
+	vertex.sort!
+	vertices[c_v] = vertex	
 end
+p AdjList.new(vertices, broken_links: :prune)
 
-adj_list = AdjList.new data
+
+#adj_list = AdjList.new data
