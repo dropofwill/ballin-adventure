71c66d2ad2a1d9602d2c2fa222f9e63f079e2601
Will <whp3652@rit.edu>
2014-06-17 20:11:13 -0400

    Document min cut in AdjList

--- a_list.rb
+++ a_list.rb
@@ -9,6 +9,8 @@ class AdjList
 		@min_cut_hash = nil
 	end
 	
+	# Find min cut by running a randomized algorithm
+	# a number of times returning the best result
 	def prob_min_cut attempts = 100
 		attempts.times do
 			find_min_cut
@@ -16,6 +18,8 @@ class AdjList
 		@min_cut
 	end
 
+	# Duplicate hash, randomly merging to adjacent 
+	# vertices until only two remain
 	def find_min_cut
 		a_hash = @vertex_hash.dup
 		while a_hash.count > 2
@@ -26,33 +30,45 @@ class AdjList
 		@min_cut = temp_min_cut if @min_cut.nil? || @min_cut > temp_min_cut
 	end
 
-	def contract_edge hash, vert1, vert2
-		# get ref to both verts
-		edges = hash[vert1] + hash[vert2]
-		vert = vert1 | vert2
-
-		# delete self loops from both ref => []
-		edges.select! do |edge|
-			!vert.include? edge
-		end
-
-		hash.delete(vert1)
-		hash.delete(vert2)
-		hash[vert] = edges
-		return hash, edges.count
-	end
-
 	def random_edge hash
 		# random vertex in @vertex_hash
 		vert1 = hash.keys.sample 
-		# random adjacent vertex 
+		# random vertex adjacent to vert1
 		vert2 = hash[vert1].sample
 
+		# Keys in the hash are sets of original vertices
+		# but the values array stores just indices, so we
+		# need to check if an already combined vertex has
+		# the same index as vert2 and if so use it instead
 		hash.keys.each { |x| vert2 = x if x.include? vert2 }
 		Set.new [vert2] unless vert2.is_a?(Set)
 		
 		return vert1, vert2
 	end
+
+	def contract_edge hash, vert1, vert2
+		# combine the two arrays of adjacent vertices
+		edges = hash[vert1] + hash[vert2]
+		# and only keep the union of the two
+		vert = vert1 | vert2
+
+		# delete edges that connect vert1 and vert2 
+		edges.select! do |edge|
+			!vert.include? edge
+		end
+		
+		# delete the old verts from the hash
+		hash.delete(vert1)
+		hash.delete(vert2)
+
+		# add the new combined vertex to the hash
+		hash[vert] = edges
+
+		# when there are just two vertices the 
+		# edge count will be the min cut
+		return hash, edges.count
+	end
+
 end
 
 
@@ -74,7 +90,7 @@ end
 #p a.vertex_hash
 
 vertices = {}
-File.open("kargerMinCut_data.txt").each_line do |line|
+File.open("data/kargerMinCut_data.txt").each_line do |line|
 	vertex = line.gsub(/\s+/, ' ').strip.split(" ")
 	vertex.map! { |i| i.to_i }
 	c_v = vertex.shift
